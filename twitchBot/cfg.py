f77fbae6b39a7fced88b5ebaaeb39397b982f5f2
Brendan Cowan <bacowan@ualberta.ca>
2017-03-29 22:25:25 -0300

    fixed setup and config

--- twitchBot/cfg.py
+++ twitchBot/cfg.py
@@ -1,14 +1,14 @@
-# cfg.py
-# from http://www.instructables.com/id/Twitchtv-Moderator-Bot/?ALLSTEPS
-HOST = "irc.chat.twitch.tv"                          # the Twitch IRC server
-PORT = 6667                                     # always use port 6667!
-NICK = "doomredbot"                             # your Twitch username, lowercase
-PASS = "oauth:token"   # your Twitch OAuth token
-CHAN = "#channel"                            # the channel you want to join
-ACCESS_TOKEN = "token"
-#URL = "https://www.bitballoon.com/api/v1/forms/58727656c4d9cc4118c68e08"
-DEFAULT_USER_LIMIT = 0
-DEFAULT_ALLOW_REPEATS = False
-HELP_COOLDOWN_SECONDS = 300 # 5 minutes
-#SUBMISSION_EXPIRY_TIME_MIN = 99999 #10
-#PAGES_PER_QUERY = 5
+import configparser
+
+config = configparser.RawConfigParser()
+config.read('cfg.cfg')
+
+HOST = config.get('twitch', 'HOST')
+PORT = config.getint('twitch', 'PORT')
+NICK = config.get('twitch', 'NICK')
+PASS = config.get('twitch', 'PASS')
+CHAN = config.get('twitch', 'CHAN')
+ACCESS_TOKEN = config.get('twitch', 'ACCESS_TOKEN')
+DEFAULT_USER_LIMIT = config.getint('bot', 'DEFAULT_USER_LIMIT')
+DEFAULT_ALLOW_REPEATS = config.getboolean('bot', 'DEFAULT_ALLOW_REPEATS')
+HELP_COOLDOWN_SECONDS = config.getint('bot', 'HELP_COOLDOWN_SECONDS')
--- twitchBot/setup.py
+++ twitchBot/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='DoomRedBot',
       version='1.0',
-      py_modules=['bot', 'cfg', 'command', 'libs', 'pipes', 'pokemonCommand', 'proxy', 'setup'],
+      install_requires=[ 'Pillow==4.0.0' ],
       dependency_links=['https://pypi.python.org/packages/c2/6c/c8d6f3d70562aa7b2b0e5a608fa8a2c884917c4eb13f203e677f8d66a8c2/Pillow-4.0.0-py2.7-win32.egg#md5=0e1facbf59d7b8e1ce020cdf886c5324']
 )
